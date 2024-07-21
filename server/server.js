d405e8c75c8af625f3e70edab45bb7ce9c8d36c6
Priyatam Reddy Somagattu <pr889078@dal.ca>
2024-07-21 02:48:11 -0300

    feat: created simple server to handle sockets for chat app

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const { Server } = require('socket.io');
+
+const io = new Server(3000, {
+    cors: {
+        origin: "*"
+    }
+});
+
+const users = {};
+
+// to create connection
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    });
+
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, userName: users[socket.id] });
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+    });
+});
